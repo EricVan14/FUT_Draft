ca8848cd66c7bf02861630d6a6b1d43be3ac55c2
EricVan14 <105650647+EricVan14@users.noreply.github.com>
2022-08-16 11:24:53 -0400

    created single player activity

--- app/src/main/java/com/example/fut_draft/MainActivity.java
+++ app/src/main/java/com/example/fut_draft/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.fut_draft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected Button singlePlayer, splitScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        singlePlayer = findViewById(R.id.singlePlayer);
+        splitScreen = findViewById(R.id.splitScreen);
+
+        singlePlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                
+
+            }
+        });
     }
 }
